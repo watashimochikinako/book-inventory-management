8985f1c0c150de89f5302ebb2767ad30e925379c
渡邉健 <t4keru@watanabekennoMacBook-Air.local>
2024-07-19 17:50:04 +0900

    DTOの追加、更新がうまくいっていない

---
+++ src/main/java/com/example/demo/application/dtos/AuthUserDTO.java
@@ -0,0 +1,35 @@
+package com.example.demo.application.dtos;
+
+/**
+ * ユーザー認証時に使用するデータ転送オブジェクト（DTO）です。
+ */
+public class AuthUserDTO {
+
+    private String email;
+    private String password;
+
+    public AuthUserDTO() {
+    }
+
+    public AuthUserDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
