87a4203ba9c3e165be46d7f12798ef6944af8ba2
渡邉健 <t4keru@watanabekennoMacBook-Air.local>
2024-08-19 11:30:17 +0900

    使用していないクラスの削除（PasswordEncoderInterface,DTO）

--- src/main/java/com/example/demo/domain/security/PasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain.security;
-
-public interface PasswordEncoder {
-    String encode(String rawPassword);
-
-    boolean matches(String rawPassword, String encodedPassword);
-}
