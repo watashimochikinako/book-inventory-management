ea19dad1767b1276b6b9721f0dc46b3c697fa1fc
渡邉健 <t4keru@watanabekennoMacBook-Air.local>
2024-08-27 17:05:24 +0900

    コメントの追記

--- src/main/java/com/example/demo/domain/security/AuthenticationProvider.java
+++ src/main/java/com/example/demo/domain/security/AuthenticationProvider.java
@@ -1,5 +1,14 @@
 package com.example.demo.domain.security;
 
+/**
+ * 現在のユーザーの認証情報を取得するためのインターフェースです。
+ */
 public interface AuthenticationProvider {
+
+    /**
+     * 現在のユーザーのメールアドレスを取得します。
+     * 
+     * @return 現在認証されているユーザーのメールアドレス
+     */
     String getCurrentUserEmail();
 }
