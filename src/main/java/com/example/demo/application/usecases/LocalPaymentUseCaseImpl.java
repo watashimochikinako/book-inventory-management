aeec63f830f2f65501f7de75a2a3c164a5c2fba7
渡邉健 <t4keru@watanabekennoMacBook-Air.local>
2024-09-13 09:43:05 +0900

    local処理を一時コメントアウト

--- src/main/java/com/example/demo/application/usecases/LocalPaymentUseCaseImpl.java
+++ src/main/java/com/example/demo/application/usecases/LocalPaymentUseCaseImpl.java
@@ -1,29 +1,29 @@
-package com.example.demo.application.usecases;
+// package com.example.demo.application.usecases;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.stereotype.Service;
 
-/**
- * ローカル環境での支払い処理に関するユースケースの実装クラスです。
- */
-@Service
-@Profile("local")
-public class LocalPaymentUseCaseImpl implements PaymentUseCase{
+// /**
+//  * ローカル環境での支払い処理に関するユースケースの実装クラスです。
+//  */
+// @Service
+// @Profile("local")
+// public class LocalPaymentUseCaseImpl implements PaymentUseCase{
 
-    /**
-     * 支払い処理をローカルデータベースで模擬的に行います。
-     *
-     * @param tokenId      支払いソースを表すトークン（例：クレジットカードトークン）
-     * @param description  支払いの説明
-     * @param amount       課金額
-     * @param currency     支払いの通貨
-     */
-    @Override
-    public void processPayment(String tokenId, String description, long amount, String currency) {
-        System.out.println("Processing payment via local database...");
-        // ローカル決済処理のコードをここに追加
-        System.out.println("Description: " + description);
-        System.out.println("Amount: " + amount);
-        System.out.println("Currency: " + currency);
-    }
-}
+//     /**
+//      * 支払い処理をローカルデータベースで模擬的に行います。
+//      *
+//      * @param tokenId      支払いソースを表すトークン（例：クレジットカードトークン）
+//      * @param description  支払いの説明
+//      * @param amount       課金額
+//      * @param currency     支払いの通貨
+//      */
+//     @Override
+//     public void processPayment(String tokenId, String description, long amount, String currency) {
+//         System.out.println("Processing payment via local database...");
+//         // ローカル決済処理のコードをここに追加
+//         System.out.println("Description: " + description);
+//         System.out.println("Amount: " + amount);
+//         System.out.println("Currency: " + currency);
+//     }
+// }
